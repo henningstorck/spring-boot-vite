acfba6c996ecd39aedac2832ea730aeee0a31997
Henning Storck <mail@henningstorck.com>
2025-06-13 08:16:46 +0200

    Reformat project

--- src/main/java/com/henningstorck/springbootvite/SpringBootViteApplication.java
+++ src/main/java/com/henningstorck/springbootvite/SpringBootViteApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootViteApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootViteApplication.class, args);
 	}
-
 }
--- src/test/java/com/henningstorck/springbootvite/SpringBootViteApplicationTests.java
+++ src/test/java/com/henningstorck/springbootvite/SpringBootViteApplicationTests.java
@@ -5,9 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringBootViteApplicationTests {
-
 	@Test
 	void contextLoads() {
 	}
-
 }
