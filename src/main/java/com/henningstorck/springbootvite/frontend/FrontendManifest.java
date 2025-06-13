2c4f1b941a12cb15fae88c7acd68c4f0441c6e25
Henning Storck <mail@henningstorck.com>
2025-06-13 12:56:56 +0200

    Embed Vite production files

---
+++ src/main/java/com/henningstorck/springbootvite/frontend/FrontendManifest.java
@@ -0,0 +1,6 @@
+package com.henningstorck.springbootvite.frontend;
+
+import java.util.HashMap;
+
+public class FrontendManifest extends HashMap<String, FrontendManifestEntry> {
+}
--- src/main/java/com/henningstorck/springbootvite/frontend/FrontendService.java
+++ src/main/java/com/henningstorck/springbootvite/frontend/FrontendService.java
@@ -1,17 +1,25 @@
 package com.henningstorck.springbootvite.frontend;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class FrontendService {
 	public static final String DEV_SERVER_HOST = "localhost";
 	public static final int DEV_SERVER_PORT = 5173;
 	public static final List<String> DEV_SERVER_ASSETS = List.of("http://localhost:5173/@vite/client", "http://localhost:5173/src/main.ts");
+
+	private final Logger logger = LoggerFactory.getLogger(FrontendService.class);
 
 	public List<String> getStylesheets() {
 		return getAssets().stream().filter(asset -> asset.endsWith(".css")).toList();
@@ -26,7 +34,27 @@ public class FrontendService {
 			return DEV_SERVER_ASSETS;
 		}
 
-		return Collections.emptyList();
+		Optional<FrontendManifest> optionalManifest = readManifest();
+
+		if (optionalManifest.isEmpty()) {
+			return Collections.emptyList();
+		}
+
+		FrontendManifest manifest = optionalManifest.get();
+		return manifest.values().stream().map(FrontendManifestEntry::file).toList();
+	}
+
+	public Optional<FrontendManifest> readManifest() {
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		try (InputStream in = getClass().getClassLoader().getResourceAsStream("static/.vite/manifest.json")) {
+			byte[] bytes = StreamUtils.copyToByteArray(in);
+			FrontendManifest manifest = objectMapper.readValue(bytes, FrontendManifest.class);
+			return Optional.of(manifest);
+		} catch (IOException e) {
+			logger.error("Cannot read manifest.", e);
+			return Optional.empty();
+		}
 	}
 
 	private boolean isDevServerRunning() {
