ee224aa17d2e2044216b065fd2a6cb459081ee29
Henning Storck <mail@henningstorck.com>
2025-06-13 08:14:23 +0200

    Initial commit

---
+++ src/main/java/com/henningstorck/springbootvite/SpringBootViteApplication.java
@@ -0,0 +1,13 @@
+package com.henningstorck.springbootvite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootViteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootViteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/henningstorck/springbootvite/SpringBootViteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.henningstorck.springbootvite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootViteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
